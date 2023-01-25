b7dd792a3bcce8f1951b8c388d42f6b37908aa83
Erich <ic19b071@technikum-wien.at>
2023-01-25 20:21:15 +0100

    initial commit

---
+++ src/main/java/com/slm/openinghours/OpeningHoursApplication.java
@@ -0,0 +1,13 @@
+package com.slm.openinghours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpeningHoursApplication.class, args);
+    }
+
+}
